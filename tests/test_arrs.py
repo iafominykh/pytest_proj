3e65ac5aebbe1b0dc5cf273b62c5694589f51bc1
iafominykh <iafominykh@yandex.ru>
2023-04-15 11:51:04 +0300

    1-commit changes

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -12,5 +12,4 @@ def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
     assert arrs.my_slice([], 1, 4) == []
-    assert arrs.my_slice([1, 2, 3], -1) == [3]
     assert arrs.my_slice([1, 2, 3], -4, 3) == [1, 2, 3]
